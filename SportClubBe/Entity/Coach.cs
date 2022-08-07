7cabc3a117cf083ad9cda9b14c63e2133c8ca758
wikto <wikto@DESKTOP-MNORIRH>
2022-08-07 17:12:55 +0200

    Dodaj pliki projektów.

---
+++ SportClubBe/Entity/Coach.cs
@@ -0,0 +1,11 @@
+﻿namespace SportClubBe.Entity
+{
+    public class Coach
+    {
+        public Guid Id { get; set; }
+        public int CoachClass { get; set; }
+        public Guid IdentityId { get; set; }
+        public virtual Identity Identity { get; set; }
+        public ICollection<CoachGroup> CoachGroups { get; set; }
+    }
+}
